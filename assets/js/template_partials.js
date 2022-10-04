7c9ccb9e3ef43ed69c8913f8899df70bee5c2db8
Menno Jak <mennojak@gmail.com>
2022-10-04 19:00:00 +0200

    favicon gefixt

--- assets/js/template_partials.js
+++ assets/js/template_partials.js
@@ -29,19 +29,3 @@ class MyFooter extends HTMLElement{
 }
 
 customElements.define('my-footer', MyFooter)
-
-
-
-// Every repeatable thing in the head portion of each page.
-class MyHeadinfo extends HTMLElement{
-	connectedCallback() {
-		this.innerHTML = `
-			<link rel="apple-touch-icon" sizes="180x180" href="icons/apple-touch-icon.png">
-			<link rel="icon" type="image/png" sizes="32x32" href="icons/favicon-32x32.png">
-			<link rel="icon" type="image/png" sizes="16x16" href="icons/favicon-16x16.png">
-			<link rel="manifest" href="icons/site.webmanifest">
-		`
-	}
-}
-
-customElements.define('my-headinfo', MyHeadinfo)
