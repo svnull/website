c3c2b71f22a6e36b9edfe64ddedc56cd18a76fb4
Menno Jak <mennojak@gmail.com>
2022-10-04 17:43:16 +0200

    Kleine refresh bug

--- assets/js/template_partials.js
+++ assets/js/template_partials.js
@@ -39,7 +39,6 @@ class MyHeadinfo extends HTMLElement{
 		
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="assets/css/main.css" />
 		`
 	}
 }
