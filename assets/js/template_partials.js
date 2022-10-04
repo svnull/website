cd442298ad835cc3ea3a178ee15238f63d6a8cd4
Menno Jak <mennojak@gmail.com>
2022-10-04 17:37:35 +0200

    Veel heel changes

--- assets/js/template_partials.js
+++ assets/js/template_partials.js
@@ -8,17 +8,15 @@ class MyFooter extends HTMLElement{
 				<div class="row">
 					<div class="col-12">
 						<ul class="social">
-							<li><a class="icon brands fa-linkedin-in" href="#"><span class="label">LinkedIn</span></a></li>
+							<li><a class="icon brands fa-linkedin-in" href="https://www.linkedin.com/company/studievereniging-null/"><span class="label">LinkedIn</span></a></li>
 							<li><a class="icon brands fa-instagram" href="https://www.instagram.com/s.v.null/"><span class="label">Instagram</span></a></li>
 							<li><a class="icon fa-envelope" href="mailto:svnull@che.nl"><span class="label">Mail</span></a></li>
 						</ul>
 
 						<div id="copyright">
 							<ul class="links">
-								<li><a href="#">Impressum</a></li>
-								<li><a href="#">Privacyverklaring</a></li>
-								<li><a href="#">Cookiebeleid</a></li>
-								<li><a href="#">Statuten</a></li>
+								<li><a href="privacy&cookies.html">Privacy & Cookies</a></li>
+								<li><a href="statuten.html">Statuten</a></li>
 							</ul>
 						</div>
 					</div>
@@ -42,7 +40,6 @@ class MyHeadinfo extends HTMLElement{
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="assets/css/main.css" />
-		
 		`
 	}
 }
