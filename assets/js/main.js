8cea43721c1dfa3673ba83f238551d3e83161c9d
Menno Jak <mennojak@gmail.com>
2022-10-16 21:23:58 +0200

    probeersel encryption

--- assets/js/main.js
+++ assets/js/main.js
@@ -87,3 +87,18 @@ function pswd(form) {
 	} 
 	else { alert("Wrong Password") } 
 }
+
+function idk(str) {
+  
+	var string = "";
+	for(var i = 0; i < str.length; i++) {
+	  var temp = str.charAt(i);
+	  if(temp !== " " || temp!== "!" || temp!== "?") {
+		 string += String.fromCharCode(13 + String.prototype.charCodeAt(temp));
+	  } else {
+		string += temp;
+	  }
+	}
+	
+	return string;
+  }
