2282955a9dfd28b280f4226ec7f3da36c1281bb3
Menno Jak <mennojak@gmail.com>
2022-10-16 21:31:12 +0200

    cipher changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,6 +72,8 @@ input.addEventListener("keypress", function(event) {
   }
 });
 
+
+// password
 function pswd(form) {
 	var somthing = idk("uryybjbeyq")
 	if (form.pass.value==somthing) { 
@@ -84,4 +86,19 @@ function pswd(form) {
 	location="https://svnull.nl/ledenpagina-protected"; 
 	} 
 	else { alert("Wrong Password") } 
+}
+
+  function idk (str) {
+    var plainText = "";
+    for(var i = 0; i < str.length; i++) {
+        var encryptedCharacter = str.charCodeAt(i);
+        if(encryptedCharacter >= 97 && encryptedCharacter <= 122) {
+            plainText += String.fromCharCode((encryptedCharacter-97 - 13 + 26) %26 + 97 );
+        } else if(encryptedCharacter >= 65 && encryptedCharacter <= 90) {
+            plainText += String.fromCharCode((encryptedCharacter-65 - 13 + 26) %26 + 65 );
+        } else {
+            plainText += String.fromCharCode(plainCharacter);
+        }
+    }
+    return plainText;
 }
