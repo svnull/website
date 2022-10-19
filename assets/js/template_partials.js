7a145e0afd3db73c9b6aa7b73d59d8ae360393fc
Menno Jak <mennojak@gmail.com>
2022-10-19 12:32:58 +0200

    Hoofdletters, typfouten en andere kleine fixes

--- assets/js/template_partials.js
+++ assets/js/template_partials.js
@@ -8,9 +8,9 @@ class MyFooter extends HTMLElement{
 				<div class="row">
 					<div class="col-12">
 						<ul class="social">
-							<li><a class="icon brands fa-linkedin-in" href="https://www.linkedin.com/company/studievereniging-null/"><span class="label">LinkedIn</span></a></li>
-							<li><a class="icon brands fa-instagram" href="https://www.instagram.com/s.v.null/"><span class="label">Instagram</span></a></li>
-							<li><a class="icon fa-envelope" href="mailto:svnull@che.nl"><span class="label">Mail</span></a></li>
+							<li><a class="icon brands fa-linkedin-in" href="https://www.linkedin.com/company/studievereniging-null/" target="_blank"><span class="label">LinkedIn</span></a></li>
+							<li><a class="icon brands fa-instagram" href="https://www.instagram.com/s.v.null/" target="_blank"><span class="label">Instagram</span></a></li>
+							<li><a class="icon fa-envelope" href="mailto:svnull@che.nl" target="_blank"><span class="label">Mail</span></a></li>
 						</ul>
 
 						<div id="copyright">
